e28642b64a66cfd2c028e170240e74cb12da6a2d
etlars <etlars@gmail.com>
2020-02-13 11:42:18 +0900

    Create http_server.py
    python http_server for test

---
+++ http_server.py
@@ -0,0 +1,22 @@
+[root@polaris01 polaris-gslb]# cat tests/http_server.py
+#!/usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class HTTPServerRequestHandler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        body = "This is test"
+        self.wfile.write(bytes(body, "utf8"))
+        return
+
+def run():
+  server_address = ('127.0.0.1', 8888)
+  httpd = HTTPServer(server_address, HTTPServerRequestHandler)
+  httpd.serve_forever()
+
+run()
