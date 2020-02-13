7bea0c61aaab59f38f64b8ceb89ffbe2ceaf8b94
etlars <etlars@gmail.com>
2020-02-13 11:44:22 +0900

    Create tcp_listen.py
    tcp_listen python for test

---
+++ tcp_listen.py
@@ -0,0 +1,20 @@
+[root@polaris01 polaris-gslb]# cat tests/tcp_listen.py
+#!/usr/bin/env python3
+
+import socket
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('127.0.0.1', 5555))
+    s.listen(1)
+
+    while 1:
+        conn, addr = s.accept()
+        #data = conn.recv(1024)
+        conn.send("blabla".encode())
+        conn.close()
+
+if __name__ == '__main__':
+    main()
+[root@polaris01 polaris-gslb]#
